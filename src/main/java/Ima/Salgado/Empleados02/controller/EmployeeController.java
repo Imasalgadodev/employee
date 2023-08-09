0c79c472cff0457b4d714528377d075ab06901c9
Imanol <imasalgadodev@gmail.com>
2023-08-09 14:25:29 -0300

    CSS styles and JavaScript are added

--- src/main/java/Ima/Salgado/Empleados02/controller/EmployeeController.java
+++ src/main/java/Ima/Salgado/Empleados02/controller/EmployeeController.java
@@ -16,58 +16,67 @@ import java.util.List;
 @RequestMapping("/employees")
 public class EmployeeController {
 
-        @Autowired
-        private EmployeeRepository employeeRepository;
+    @Autowired
+    private EmployeeRepository employeeRepository;
 
-        @GetMapping
-        public List<Employee> getAllEmployee() {
-            return employeeRepository.findAll();
+    @GetMapping
+    public List<Employee> getAllEmployee() {
+        return employeeRepository.findAll();
+    }
+
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id).orElse(null);
+        if (employee == null) {
+            return ResponseEntity.notFound().build();
         }
+        return ResponseEntity.ok(employee);
+    }
 
-        @PostMapping
-        public Employee createEmployee(@RequestBody Employee employee) {
-            return employeeRepository.save(employee);
+    @PutMapping("/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee updatedemployee) {
+        Employee employee = employeeRepository.findById(id).orElse(null);
+        if (employee == null) {
+            return ResponseEntity.notFound().build();
         }
+        employee.setName(updatedemployee.getName());
+        employee.setLastName(updatedemployee.getLastName());
 
-        @GetMapping("/{id}")
-        public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
-            Employee employee = employeeRepository.findById(id).orElse(null);
-            if (employee == null) {
-                return ResponseEntity.notFound().build();
-            }
-            return ResponseEntity.ok(employee);
+        employeeRepository.save(employee);
+        return ResponseEntity.ok(employee);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEmployee(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id).orElse(null);
+        if (employee == null) {
+            return ResponseEntity.notFound().build();
         }
-
-        @PutMapping("/{id}")
-        public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee updatedemployee) {
-            Employee employee = employeeRepository.findById(id).orElse(null);
-            if (employee == null) {
-                return ResponseEntity.notFound().build();
-            }
-            employee.setName(updatedemployee.getName());
-            employee.setLastName(updatedemployee.getLastName());
-
-            employeeRepository.save(employee);
-            return ResponseEntity.ok(employee);
-        }
-
-        @DeleteMapping("/{id}")
-        public ResponseEntity<Void> deleteEmployee(@PathVariable Long id) {
-            Employee employee = employeeRepository.findById(id).orElse(null);
-            if (employee == null) {
-                return ResponseEntity.notFound().build();
-            }
-            employeeRepository.delete(employee);
-            return ResponseEntity.noContent().build();
-        }
+        employeeRepository.delete(employee);
+        return ResponseEntity.noContent().build();
+    }
 
     @Autowired
     private EmployeeService employeeService;
 
     @GetMapping("/list")
-    public String getAllEmployees(Model model) {
+    public String getAllEmployees(Model model, @RequestParam(defaultValue = "asc") String sort) {
         List<Employee> employees = employeeService.getAllEmployees();
+
+        if ("desc".equals(sort)) {
+            employees.sort((e1, e2) -> e2.getName().compareTo(e1.getName()));
+        } else {
+            employees.sort((e1, e2) -> e1.getName().compareTo(e2.getName()));
+        }
+
         model.addAttribute("employees", employees);
+        model.addAttribute("sort", sort);
         return "employees";
     }
 }
+
